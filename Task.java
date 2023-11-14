ce2490a909fd3bb459f3ca71bbae5366884b26be
aufazaikra <128202887+aufazaikra@users.noreply.github.com>
2023-11-14 22:58:29 +0700

    Update class Task.java

--- Task.java
+++ Task.java
@@ -1,1 +1,18 @@
+abstract class Task {
+    private String taskName;
+    private int taskId;
+    private static int lastTaskId = 0;
 
+    public Task(String name) {
+        this.taskName = name;
+        this.taskId = ++lastTaskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public int getTaskId() {
+        return taskId;
+    }
+}
