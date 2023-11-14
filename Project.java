d094aa6f184fb80d10860ffef4073b617082b320
aufazaikra <128202887+aufazaikra@users.noreply.github.com>
2023-11-14 23:03:19 +0700

    Update Project.java
    Menambahkan keyword super

--- Project.java
+++ Project.java
@@ -2,8 +2,10 @@ class Project extends Task {
     private String projectDetails;
 
     public Project(String name, String details) {
+        super(name);
         this.projectDetails = details;
     }
+    
     public String getProjectDetails() {
         return projectDetails;
     }
