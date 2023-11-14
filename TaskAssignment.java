cd5bd41dcf843408b7f9bf2d658463116c9d1607
farhansyaah28 <129988561+farhansyaah28@users.noreply.github.com>
2023-11-14 23:10:44 +0700

    Update TaskAssignment.java

--- TaskAssignment.java
+++ TaskAssignment.java
@@ -1,1 +1,5 @@
-
+public class TaskAssignment {
+    public void assignTaskToEmployee(Employee employee, Task task) {
+        System.out.println("Assigning task " + task.getTaskName() + " to " + employee.getEmployeeName());
+    }
+}
