58c855f7221ec143a41cb8de1a5063c339b98ae7
farhansyaah28 <129988561+farhansyaah28@users.noreply.github.com>
2023-11-15 19:54:37 +0700

    Update TaskAssignment.java

--- TaskAssignment.java
+++ TaskAssignment.java
@@ -1,5 +1,14 @@
-public class TaskAssignment {
+import java.util.ArrayList;
+
+class TaskAssignment {
+    private ArrayList<Task> assignedTasks;
+
+    public TaskAssignment() {
+        assignedTasks = new ArrayList<>();
+    }
+
     public void assignTaskToEmployee(Employee employee, Task task) {
-        System.out.println("Assigning task " + task.getTaskName() + " to " + employee.getEmployeeName());
+        System.out.println("Tugas " + task.getTaskName() + " ditugaskan kepada " + employee.getEmployeeName());
+        assignedTasks.add(task);
     }
 }
