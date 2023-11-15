40b25d1e10d42812631b04395fedb619a0a1f5e7
farhansyaah28 <129988561+farhansyaah28@users.noreply.github.com>
2023-11-15 19:53:11 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,83 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // Input untuk tugas proyek
+        System.out.print("Masukkan nama tugas proyek: ");
+        String projectTaskName = scanner.nextLine();
+        System.out.print("Masukkan detail proyek: ");
+        String projectDetails = scanner.nextLine();
+        Project projTask = new Project(projectTaskName, projectDetails);
+
+        // Input untuk karyawan
+        System.out.print("Masukkan nama karyawan: ");
+        String employeeName = scanner.nextLine();
+        Employee empTask = new Employee("Tugas Karyawan", employeeName);
+
+        // Input untuk tugas karyawan
+        System.out.print("Masukkan tugas karyawan: ");
+        String subTaskName = scanner.nextLine();
+        SubTask subTask = new SubTask(subTaskName);
+
         TaskManager taskManager = new TaskManager();
-        char addMoreTasks;
+        taskManager.addTask(projTask);
+        taskManager.addTask(empTask);
+        taskManager.addTask(subTask);
 
-        do {
-           System.out.print("Enter task name: ");
-            String taskName = scanner.nextLine();
-            System.out.print("Enter task ID: ");
-            int taskId = scanner.nextInt();
-
-            scanner.nextLine(); // Clearing newline
-
-            System.out.print("Choose task type (Employee/Project/SubTask): ");
-            String taskType = scanner.nextLine();
-
-            switch (taskType.toLowerCase()) {
-                case "employee":
-                    System.out.print("Enter employee name: ");
-                    String employeeName = scanner.nextLine();
-                    Employee empTask = new Employee(taskName, taskId, employeeName);
-                    taskManager.addTask(empTask);
-                    break;
-                case "project":
-                    System.out.print("Enter project details: ");
-                    String projectDetails = scanner.nextLine();
-                    Project projTask = new Project(taskName, taskId, projectDetails);
-                    taskManager.addTask(projTask);
-                    break;
-                case "subtask":
-                    SubTask subTask = new SubTask(taskName, taskId);
-                    taskManager.addTask(subTask);
-                    break;
-                default:
-                    System.out.println("Invalid task type.");
-                    break;
-            }
-
-
-            System.out.print("Add another task? (y/n): ");
-            addMoreTasks = scanner.next().charAt(0);
-            scanner.nextLine(); // Clearing newline
-
-        } while (addMoreTasks == 'y');
-
-        System.out.println("Task List:");
+        System.out.println("\nDaftar Tugas:");
         taskManager.displayTasks();
 
-        TaskAssignMultiple taskAssignMultiple = new TaskAssignMultiple();
-        if (!taskManager.getTasks().isEmpty()) {
-    Employee lastEmployee = null;
-    List<SubTask> subTasks = new ArrayList<>();
+        TaskAssignment taskAssignment = new TaskAssignment();
 
-    for (Task task : taskManager.getTasks()) {
-        if (task instanceof Employee) {
-            lastEmployee = (Employee) task;
-        } else if (task instanceof SubTask) {
-            subTasks.add((SubTask) task);
-        }
-    }
-
-     if (lastEmployee != null) {
-        taskManager.getTasks().get(taskManager.getTasks().size() - 1).setAssignee(lastEmployee.getEmployeeName());
-    } else {
-        if (lastEmployee == null) {
-            System.out.println("No employee available to assign multiple tasks.");
-        }
-        if (subTasks.isEmpty()) {
-            System.out.println("No sub-tasks available to assign to an employee.");
-        }
-    }
-} else {
-    System.out.println("No tasks to assign.");
-}
-
-        scanner.close();
+        // // Input untuk menugaskan tugas karyawan
+        // System.out.print("\nMasukkan nama karyawan yang akan ditugaskan tugas: ");
+        // String assignedEmployeeName = scanner.nextLine();
+        taskAssignment.assignTaskToEmployee(empTask, subTask);
     }
 }
